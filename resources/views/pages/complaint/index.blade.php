bf118b2bb4e7290c921263c4da77e63614a2a687
dedyprasetyoutomo <dedyihuy21@gmail.com>
2025-11-11 10:37:47 +0700

    menambahkan sourcecode 11-11-2025 10:37 wib

---
+++ resources/views/pages/complaint/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+     <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Aduan</h1>
+                        <a href="/complaint/create" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-plus fa-sm text-white-50"></i> Buat Aduan</a>
+                    </div>
+                    {{-- Table --}}
+                    <div class="row">
+                        <div class="col">
+                            <div class="card shadow">
+                                <div class="card-body">
+                                    <table class="table table-responsive table-bordered table-hovered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Judul</th>
+                                            <th>Isi Aduan</th>
+                                            <th>Status</th>
+                                            <th>Foto Bukti</th>
+                                            <th>Tanggal Laporan</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    @if (count($complaints) < 1)
+                                    <tbody>
+                                        <tr>
+                                            <td colspan="11">
+                                                <p class="pt-3 text-center">Tidak ada data</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    @else
+                                            <tbody>
+                                        @foreach ($complaints as $item)
+                                            <tr>
+                                            <td>{{ $loop->iteration + $complaints->firstItem() - 1 }}</td>
+                                            <td>{{ $item->title }}</td>
+                                            <td>{!! wordwrap($item->content,50,"<br>\n") !!}</td>
+                                            <td>{{ $item->status }}</td>
+                                            <td>{{ $item->photo_proof }}</td>
+                                            <td>{{ $item->report_date }}</td>
+                                            <td>
+                                                <div class="d-flex align-items-center" style="gap : 10px;">
+                                                    <a href="/complaint/{{ $item->id }}" class="d-inline-block btn btn-sm btn-warning">
+                                                    <i class="fas fa-pen"></i>
+                                                    </a>
+                                                     <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationDelete-{{ $item->id }}">
+                                                    <i class="fas fa-eraser"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @include('pages.complaint.confirmation-delete')
+                                        @endforeach
+                                         
+                                    </tbody>
+                                    @endif
+                                    
+                                    </table>
+                                </div>
+                                @if ($complaints->lastPage() > 1)
+                                <div class="card-footer">
+                                    {{ $complaints->links('pagination::bootstrap-5') }}
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+@endsection
